7413a1be8fa62a1c611ea38972f3d86e3ca8a18b
chelseajia <jiahuibo@seattleu.edu>
2019-05-26 23:46:10 -0700

    implement Recursive Feature Elimination

--- AA_modeling.py
+++ AA_modeling.py
@@ -20,3 +20,18 @@ X = acs.drop(['Airport'],axis = 1)
 # Logit model 
 logit_model = sm.Logit(Y, X).fit()
 logit_model.summary()
+
+# Recursive Feature Elimination
+# Reference: https://www.datacamp.com/community/tutorials/feature-selection-python
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+
+# Feature extraction
+model = LogisticRegression()
+rfe = RFE(model, 3)
+fit = rfe.fit(X, Y)
+print("Num Features: %s" % (fit.n_features_))
+print("Selected Features: %s" % (fit.support_))
+print("Feature Ranking: %s" % (fit.ranking_))  
+# Top 3 features: Destination, DepartureTime, NoTransport
+
