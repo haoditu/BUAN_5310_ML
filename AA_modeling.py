7ec451067d94bd8657ecd2b1f08976fa9e801653
chelseajia <jiahuibo@seattleu.edu>
2019-06-09 00:27:06 -0700

    Update Models
    decision tree models and SVM models updated

--- AA_modeling.py
+++ AA_modeling.py
@@ -1,188 +1,151 @@
 import pandas as pd
-import statsmodels.api as sm
-from sklearn.model_selection  import train_test_split
 from sklearn import metrics
 from sklearn import tree
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC 
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
 import os
 
 #os.chdir("/Users/Shenshen_Wu/Documents/GitHub/BUAN_5310_ML")
 
 
-acs_airport = pd.read_csv("acs_ap.csv", sep=',', index_col=0)
-acs_airline = pd.read_csv("acs_al.csv", sep=',', index_col=0)
 
-# Summary statistics
-pd.set_option('display.max_columns', None)
-acs_airport.describe()
-acs_airline.describe()
+#### Airport Modeling
+## data preprocessing
+train_ap = pd.read_csv("train_airport.csv", sep=',', index_col=0)
+test_ap = pd.read_csv("test_airport.csv", sep=',', index_col=0 )
 
 
-# Correlation
-acs_airport.corr()
-acs_airline.corr()
+X_train_ap = train_ap.drop(['Airport'],axis = 1)
+Y_train_ap = train_ap['Airport'].astype('category').cat.codes
 
-### Airport modeling
-# Covert categorical variables to numerical
-acs_airport.insert(0, 'intercept', 1.0)
-acs_airport['Airport'] = acs_airport['Airport'].astype('category').cat.codes
-acs_airport['Airline'] = acs_airport['Airline'].astype('category').cat.codes
-#acs_airport['TripPurpose'] = acs_airport['TripPurpose'].astype('category').cat.codes
-acs_airport['Age'] = acs_airport['Age'].astype('category').cat.codes
-acs_airport['Income'] = acs_airport['Income'].astype('category').cat.codes
-acs_airport['ModeTransport'] = acs_airport['ModeTransport'].astype('category').cat.codes
-acs_airport['Occupation'] = acs_airport['Occupation'].astype('category').cat.codes
-acs_airport['Occupation'] = acs_airport['Occupation'].astype('category').cat.codes
-acs_airport['Nationality'] = acs_airport['Nationality'].astype('category').cat.codes
+X_test_ap = test_ap.drop(['Airport'],axis = 1)
+Y_test_ap = test_ap['Airport'].astype('category').cat.codes
 
+X_train_ap["Airline"] = X_train_ap["Airline"].astype('category').cat.codes
+X_train_ap["Nationality"] = X_train_ap["Nationality"].astype('category').cat.codes
+X_train_ap["TripPurpose"] = X_train_ap["TripPurpose"].astype('category').cat.codes
+X_train_ap["FrequentFlightDestination"] = X_train_ap["FrequentFlightDestination"].astype('category').cat.codes
+X_train_ap["Destination"] = X_train_ap["Destination"].astype('category').cat.codes
+X_train_ap["Occupation"] = X_train_ap["Occupation"].astype('category').cat.codes
+X_train_ap["Income"] = X_train_ap["Income"].astype('category').cat.codes
 
-Y_ap= acs_airport['Airport']
-X_ap= acs_airport.drop(['Airport'],axis = 1)
-
-# Logit model 
-X_train, X_test, Y_train, Y_test = train_test_split(X_ap, Y_ap, test_size=0.30,random_state=109)
-result = sm.Logit(Y_train, X_train).fit()
-result.summary()
-Y_pred = result.predict(X_test)
-X_test.loc[:,'prediction']=0
-X_test.loc[Y_pred > 0.5,'prediction']=1
-pd.crosstab(Y_test,X_test['prediction'],rownames =['actual'],colnames=['predicted'])
+X_test_ap["Airline"] = X_test_ap["Airline"].astype('category').cat.codes
+X_test_ap["Nationality"] = X_test_ap["Nationality"].astype('category').cat.codes
+X_test_ap["TripPurpose"] = X_test_ap["TripPurpose"].astype('category').cat.codes
+X_test_ap["FrequentFlightDestination"] = X_test_ap["FrequentFlightDestination"].astype('category').cat.codes
+X_test_ap["Destination"] = X_test_ap["Destination"].astype('category').cat.codes
+X_test_ap["Occupation"] = X_test_ap["Occupation"].astype('category').cat.codes
+X_test_ap["Income"] = X_test_ap["Income"].astype('category').cat.codes
 
 
-#print(metrics.confusion_matrix(Y_test, Y_pred))  
-#print(classification_report(Y_test, Y_pred))
-#print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-#print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
-
-# Recursive Feature Elimination
-# Reference: https://www.datacamp.com/community/tutorials/feature-selection-python
-#from sklearn.feature_selection import RFE
-#from sklearn.linear_model import LogisticRegression
-
-# Feature ranking and extraction
-model = LogisticRegression()
-rfe = RFE(model, 5)
-fit = rfe.fit(X_ap, Y_ap)
-print("Num Features: %s" % (fit.n_features_))
-print("Selected Features: %s" % (fit.support_))
-print("Feature Ranking: %s" % (fit.ranking_))  
-# Top 5 features: Airline,Age, Destination, DepartureTime, Income
-
-
-### Tree Model
-X_train, X_test, Y_train, Y_test = train_test_split(X_ap, Y_ap, test_size=0.30,random_state=109)  
+### Tree Model 1
 clf = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=10, max_features=None, max_leaf_nodes=None, min_samples_leaf=10, min_samples_split=2, min_weight_fraction_leaf=0.0, presort=False, random_state=100, splitter='best')
-clf = clf.fit(X_train, Y_train)
-# export estimated tree into dot graphic file
-dot_data = tree.export_graphviz(clf, out_file='Dtree_airport.dot', feature_names=X_ap.columns)
-Y_pred = clf.predict(X_test)  
-print(metrics.confusion_matrix(Y_test, Y_pred))  
-print(classification_report(Y_test, Y_pred))
-print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
+clf = clf.fit(X_train_ap, Y_train_ap)
 
-# change tree parameters
-X_train, X_test, Y_train, Y_test = train_test_split(X_ap, Y_ap, test_size=0.30,random_state=109)  
+# export estimated tree into dot graphic file
+dot_data = tree.export_graphviz(clf, out_file='Dtree_airport_1.dot', feature_names=X_train_ap.columns)
+Y_pred_ap = clf.predict(X_test_ap) 
+print("Train Accuracy:", metrics.accuracy_score(Y_train_ap, clf.predict(X_train_ap)))
+print("Test Accuracy:", metrics.accuracy_score(Y_test_ap, Y_pred_ap))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_ap, Y_pred_ap))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_ap, Y_pred_ap)) 
+print(classification_report(Y_test_ap, Y_pred_ap))
+
+# Tree Model 2: change tree parameters 
 clf = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=5, max_features=None, max_leaf_nodes=None, min_samples_leaf=3, min_samples_split=3, min_weight_fraction_leaf=0.0, presort=False, random_state=100, splitter='best')
-clf = clf.fit(X_train, Y_train)
-Y_pred = clf.predict(X_test)  
-print(metrics.confusion_matrix(Y_test, Y_pred)) 
-print(classification_report(Y_test, Y_pred)) 
-print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
+clf = clf.fit(X_train_ap, Y_train_ap) 
+
+# export estimated tree into dot graphic file
+dot_data = tree.export_graphviz(clf, out_file='Dtree_airport_2.dot', feature_names=X_train_ap.columns)
+Y_pred_ap = clf.predict(X_test_ap) 
+print("Train Accuracy:", metrics.accuracy_score(Y_train_ap, clf.predict(X_train_ap)))
+print("Test Accuracy:", metrics.accuracy_score(Y_test_ap, Y_pred_ap))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_ap, Y_pred_ap))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_ap, Y_pred_ap)) 
+print(classification_report(Y_test_ap, Y_pred_ap))
 
 
 ### SVM model
-X_train, X_test, y_train, y_test = train_test_split(X_ap, Y_ap, test_size=0.30,random_state=109)  
-
 # Normalize data
 scaler = StandardScaler()  
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)  
-X_test = scaler.transform(X_test) 
+scaler.fit(X_train_ap)
+X_train_ap = scaler.transform(X_train_ap)  
+X_test_ap = scaler.transform(X_test_ap) 
 svclassifier = SVC(kernel='linear')    	
-svclassifier.fit(X_train, y_train)  
-y_pred = svclassifier.predict(X_test)  	
-print(metrics.confusion_matrix(y_test, y_pred)) 	
-print(classification_report(y_test, y_pred))
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-print("Test Erro Rate:",1-metrics.accuracy_score(y_test, y_pred))
+svclassifier.fit(X_train_ap, Y_train_ap)  
+y_pred_ap = svclassifier.predict(X_test_ap)  	
+print(metrics.confusion_matrix(Y_test_ap, y_pred_ap)) 	
+print(classification_report(Y_test_ap, y_pred_ap))
+print("Train Accuracy:", metrics.accuracy_score(Y_train_ap, svclassifier.predict(X_train_ap)))
+print("Test Accuracy:",metrics.accuracy_score(Y_test_ap, y_pred_ap))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_ap, y_pred_ap))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_ap, Y_pred_ap)) 
+
+#### Airline modeling
+## data preprocessing
+train_al = pd.read_csv("train_airline.csv", sep=',', index_col=0)
+test_al = pd.read_csv("test_airline.csv", sep=',', index_col=0 )
 
 
-### Airline modeling
-# Covert categorical variables to numerical
-acs_airline['Airport'] = acs_airport['Airport'].astype('category').cat.codes
-acs_airline['Airline'] = acs_airport['Airline'].astype('category').cat.codes
-acs_airline['TripPurpose'] = acs_airport['TripPurpose'].astype('category').cat.codes
-acs_airline['Age'] = acs_airport['Age'].astype('category').cat.codes
-acs_airline['Income'] = acs_airport['Income'].astype('category').cat.codes
-Y_al= acs_airport['Airline']
-X_al= acs_airport.drop(['Airline', 'ProvinceResidence'], axis = 1)
+X_train_al = train_al.drop(['Airline'],axis = 1)
+Y_train_al = train_al['Airline']
 
-# Logit model 
-logit_model = sm.Logit(Y_al, X_al).fit()
-logit_model.summary()
+X_test_al = test_al.drop(['Airline'],axis = 1)
+Y_test_al = test_al['Airline']
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_al, Y_al, test_size=0.30,random_state=109)
-logisticRegr = LogisticRegression()
-logisticRegr.fit(X_train, Y_train)
-Y_pred = logisticRegr.predict(X_test)
-print(metrics.confusion_matrix(Y_test, Y_pred))  
-print(classification_report(Y_test, Y_pred))
-print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
+X_train_al["Nationality"] = X_train_al["Nationality"].astype('category').cat.codes
+X_train_al["TripPurpose"] = X_train_al["TripPurpose"].astype('category').cat.codes
+X_train_al["FrequentFlightDestination"] = X_train_al["FrequentFlightDestination"].astype('category').cat.codes
+X_train_al["SeatClass"] = X_train_al["SeatClass"].astype('category').cat.codes
+X_train_al["Income"] = X_train_al["Income"].astype('category').cat.codes
 
-# Feature ranking and extraction
-model = LogisticRegression()
-rfe = RFE(model, 5)
-fit = rfe.fit(X_al, Y_al)
-print("Num Features: %s" % (fit.n_features_))
-print("Selected Features: %s" % (fit.support_))
-print("Feature Ranking: %s" % (fit.ranking_))  
-# Top 5 features: Airport,NoTripLastYear,DepartureTime,FlyingCompany,GroupTravel
+X_test_al["Nationality"] = X_test_al["Nationality"].astype('category').cat.codes
+X_test_al["TripPurpose"] = X_test_al["TripPurpose"].astype('category').cat.codes
+X_test_al["FrequentFlightDestination"] = X_test_al["FrequentFlightDestination"].astype('category').cat.codes
+X_test_al["SeatClass"] = X_test_al["SeatClass"].astype('category').cat.codes
+X_test_al["Income"] = X_test_al["Income"].astype('category').cat.codes
 
 
-### Tree Model
-X_train, X_test, Y_train, Y_test = train_test_split(X_al, Y_al, test_size=0.30,random_state=109)  
+### Tree Model 1
 clf = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=10, max_features=None, max_leaf_nodes=None, min_samples_leaf=10, min_samples_split=2, min_weight_fraction_leaf=0.0, presort=False, random_state=100, splitter='best')
-clf = clf.fit(X_train, Y_train)
+clf = clf.fit(X_train_al, Y_train_al)
+
 # export estimated tree into dot graphic file
-dot_data = tree.export_graphviz(clf, out_file='Dtree_airline1.dot', feature_names=X_al.columns)
-Y_pred = clf.predict(X_test)  
-print(metrics.confusion_matrix(Y_test, Y_pred))  
-print(classification_report(Y_test, Y_pred))
-print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
+dot_data = tree.export_graphviz(clf, out_file='Dtree_airline_1.dot', feature_names=X_train_al.columns)
+Y_pred_al = clf.predict(X_test_al) 
+print("Train Accuracy:", metrics.accuracy_score(Y_train_al, clf.predict(X_train_al)))
+print("Test Accuracy:", metrics.accuracy_score(Y_test_al, Y_pred_al))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_al, Y_pred_al))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_al, Y_pred_al)) 
+print(classification_report(Y_test_al, Y_pred_al))
 
-# change tree parameters
-X_train, X_test, Y_train, Y_test = train_test_split(X_al, Y_al, test_size=0.30,random_state=109)  
+# Tree Model 2: change tree parameters 
 clf = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=5, max_features=None, max_leaf_nodes=None, min_samples_leaf=3, min_samples_split=3, min_weight_fraction_leaf=0.0, presort=False, random_state=100, splitter='best')
-clf = clf.fit(X_train, Y_train)
-Y_pred = clf.predict(X_test)  
-print(metrics.confusion_matrix(Y_test, Y_pred)) 
-print(classification_report(Y_test, Y_pred)) 
-print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
-print("Error Rate:",1-metrics.accuracy_score(Y_test, Y_pred))
+clf = clf.fit(X_train_al, Y_train_al)
 
+# export estimated tree into dot graphic file
+dot_data = tree.export_graphviz(clf, out_file='Dtree_airline_1.dot', feature_names=X_train_al.columns)
+Y_pred_al = clf.predict(X_test_al) 
+print("Train Accuracy:", metrics.accuracy_score(Y_train_al, clf.predict(X_train_al)))
+print("Test Accuracy:", metrics.accuracy_score(Y_test_al, Y_pred_al))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_al, Y_pred_al))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_al, Y_pred_al)) 
+print(classification_report(Y_test_al, Y_pred_al))
 
 ### SVM model
-X_train, X_test, y_train, y_test = train_test_split(X_al, Y_al, test_size=0.30,random_state=109)  
-
 # Normalize data
 scaler = StandardScaler()  
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)  
-X_test = scaler.transform(X_test) 
+scaler.fit(X_train_al)
+X_train_al = scaler.transform(X_train_al)  
+X_test_al = scaler.transform(X_test_al) 
 svclassifier = SVC(kernel='linear')    	
-svclassifier.fit(X_train, y_train)  
-y_pred = svclassifier.predict(X_test)  	
-print(metrics.confusion_matrix(y_test, y_pred)) 	
-print(classification_report(y_test, y_pred))
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-print("Test Erro Rate:",1-metrics.accuracy_score(y_test, y_pred))
+svclassifier.fit(X_train_al, Y_train_al)  
+y_pred_al = svclassifier.predict(X_test_al)  	
+print(metrics.confusion_matrix(Y_test_al, y_pred_al)) 	
+print(classification_report(Y_test_al, y_pred_al))
+print("Train Accuracy:", metrics.accuracy_score(Y_train_al, svclassifier.predict(X_train_al)))
+print("Test Accuracy:",metrics.accuracy_score(Y_test_al, y_pred_al))
+print("Test Error Rate:",1-metrics.accuracy_score(Y_test_al, y_pred_al))
+print("Confusion Matrix:", metrics.confusion_matrix(Y_test_al, Y_pred_al)) 
 
